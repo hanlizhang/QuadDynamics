d9dd5e356153b9a35df23f0c6a81b40bced5bdf7
hanlizhang <hanlizh@seas.upenn.edu>
2023-08-23 22:16:12 -0400

    modify inp_size(input trajs' dimension)num_hidden

--- learning/mlp.py
+++ learning/mlp.py
@@ -24,43 +24,43 @@ class MLP(nn.Module):
     num_outputs: int
 
     def setup(self):
-        self.linear = [nn.Dense(features=self.num_hidden[i]) for i in range(len(self.num_hidden))]
+        self.linear = [
+            nn.Dense(features=self.num_hidden[i]) for i in range(len(self.num_hidden))
+        ]
         self.linear2 = nn.Dense(features=self.num_outputs)
 
     def __call__(self, x):
         for i in range(len(self.num_hidden)):
-            #import pdb;
-            #pdb.set_trace()
+            # import pdb;
+            # pdb.set_trace()
             x = self.linear[i](x)
             x = nn.elu(x)
         x = self.linear2(x)
         return x
-        #return x ** 2
+        # return x ** 2
 
 
 class MLP_torch(tnn.Module):
-    def __init__(self,
-                 inp_size=1002,
-                 num_hidden=[500, 400, 200]):
-      super(MLP_torch, self).__init__()
+    def __init__(self, inp_size=501, num_hidden=[50, 40, 20]):
+        super(MLP_torch, self).__init__()
 
-      self.inp_size = inp_size
-      self.num_hidden = num_hidden
+        self.inp_size = inp_size
+        self.num_hidden = num_hidden
 
-      # Hidden layers
-      self.hidden = tnn.ModuleList()
-      self.hidden.append(tnn.Linear(inp_size, num_hidden[0]))
-      for k in range(len(num_hidden) - 1):
-        self.hidden.append(tnn.Linear(num_hidden[k], num_hidden[k+1]))
+        # Hidden layers
+        self.hidden = tnn.ModuleList()
+        self.hidden.append(tnn.Linear(inp_size, num_hidden[0]))
+        for k in range(len(num_hidden) - 1):
+            self.hidden.append(tnn.Linear(num_hidden[k], num_hidden[k + 1]))
 
-      # Final output layer
-      self.linear2 = tnn.Linear(num_hidden[-1], 1)
+        # Final output layer
+        self.linear2 = tnn.Linear(num_hidden[-1], 1)
 
     def forward(self, x):
         x = x.float()
         for i in range(len(self.num_hidden)):
-            #import ipdb;
-            #ipdb.set_trace()
+            # import ipdb;
+            # ipdb.set_trace()
             x = self.hidden[i](x)
             x = F.elu(x)
         x = self.linear2(x)

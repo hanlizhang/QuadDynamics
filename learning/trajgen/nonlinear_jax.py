bd105ca711cab373cb6c9b70fc867e613f3a64df
hanlizhang <hanlizh@seas.upenn.edu>
2023-09-19 00:36:04 -0400

    run inference_jax_0909 to get results

--- learning/trajgen/nonlinear_jax.py
+++ learning/trajgen/nonlinear_jax.py
@@ -50,8 +50,12 @@ def modify_reference(
     # cost_mat_full = spl.block_diag(*[cost_mat for i in range(p)])
     # A_coeff_full = spl.block_diag(*[A_coeff for i in range(p)])
     # b_coeff_full = jnp.ravel(b_coeff)
+    times = np.concatenate(
+        [np.linspace(0, ts[i + 1] - ts[i], numsteps // num_seg) for i in range(num_seg)]
+    )
+    print("Times", times.shape)
 
-    # @jit
+    ## @jit
     def nn_cost(coeffs):
         """
         Function to compute trajectories given polynomial coefficients
@@ -60,6 +64,26 @@ def modify_reference(
         :param numsteps: Total number of samples in the reference
         :return: ref
         """
+        ref = jnp.zeros((p, numsteps))
+
+        for j in range(p):
+            # ref.at[:, i].set(jnp.dot(_diff_coeff(coeffs.shape[2] - 1, tt - ts[k], 0), coeffs[:, k, :].T))
+            ref = ref.at[j, :].set(
+                jnp.polyval(
+                    coeffs[j * num_seg * (order + 1) : (j + 1) * num_seg * (order + 1)],
+                    times,
+                )
+            )
+            # ref.append(jnp.polyval(coeffs[j, :], tt - ts[k]))
+            # ref.append(poly[k](tt - ts[k]))
+        # print("Network cost", jnp.exp(regularizer(jnp.append(wp[0, :], jnp.vstack(ref)))[0]))
+        # print("Coeff cost", (coeffs.T @ cost_mat_full @ coeffs))
+        return coeffs.T @ cost_mat_full @ coeffs + jnp.exp(
+            regularizer(jnp.append(wp[0, :], jnp.vstack(ref)))[0]
+        )
+        # for j in range(p):
+        #    ref = ref.at[j, :].set(jnp.polyval(coeffs[j * num_seg * (order + 1):(j + 1) * num_seg * (order + 1)], times))
+        # return coeffs.T @ cost_mat_full @ coeffs + regularizer(jnp.append(wp[0, :], jnp.vstack(ref)))[0]
 
         # for j in range(p):
         #     ref.at[j, :].set(
@@ -97,7 +121,7 @@ def modify_reference(
         # )
         print("Cost matrix", cost_mat_full)
         print("Coeff  cost", coeffs.T @ cost_mat_full @ coeffs)
-        return coeffs.T @ cost_mat_full @ coeffs
+        # return coeffs.T @ cost_mat_full @ coeffs
         # return jnp.exp(
         #     regularizer(
         #         jnp.append(
@@ -139,8 +163,12 @@ def modify_reference(
     print("coeff0_copy.ravel's first 8", coeff0_copy.ravel()[:8])"""
     sol = pg.run(coeff0, hyperparams_proj=(A_coeff_full, b_coeff_full))
     # sol = pg.run(coeff0)
+    # pg = ProjectedGradient(nn_cost, projection=projection_affine_set)
+    # sol = pg.run(coeff0.ravel(), hyperparams_proj=(A_coeff_full, b_coeff_full))
     coeff = sol.params
     pred = sol.state.error
+
+    print("Norm difference", np.linalg.norm(coeff0.ravel() - coeff))
 
     return coeff, pred
     # return np.reshape(coeff, (p, num_seg, order + 1)), pred
